34095bb9f4e07a5ac9f73812bac74161f27c48ae
Shraddha Apshankar <shraddha@Shraddhas-MacBook-Pro.local>
2020-08-25 16:40:46 -0700

    updates

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "dist")));
+app.use('/images', express.static(path.join(__dirname, "images")));
+app.get('/', (req, res) => res.send('Shraddha says Hi!'));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, "dist", "index.html")));
+
+console.log('Starting server on 8080...');
+app.listen(8080);
