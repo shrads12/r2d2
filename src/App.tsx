3220c568583385deee2ebf0c420271d5563c4d4a
Shraddha Apshankar <shraddha@Shraddhas-MacBook-Pro.local>
2020-08-26 14:23:19 -0700

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './app.css';
+import './App.css';
 import Game from './Game';
 
 ReactDOM.render(  <React.StrictMode><Game /></React.StrictMode>, document.getElementById("main"));
